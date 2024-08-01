8981b6d61cf86b8da32eed674f86d98fbc982dc1
akashmunagala7 <23eg507b05@anurag.edu.in>
2024-08-01 14:37:24 +0530

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,6 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname,'app_server','views'));
 app.set('view engine', 'jade');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
